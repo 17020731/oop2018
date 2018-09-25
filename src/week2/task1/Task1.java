5fb1fc9a709394bca20ef711ef0f6cfe58ec7688
17020731 <37697235+17020731@users.noreply.github.com>
2018-09-25 22:49:37 +0700

    1

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,22 +1,22 @@
-package week2.task1;
+package week2;
 
 public class Task1 {
 
     public static int gcd(int a, int b) {
         if(a==b)
-        	return a;
+            return a;
         else if( a>b)
-        	return gcd(a-b,b);
+            return gcd(a-b,b);
         else
-        	return gcd(a,b-a);
+            return gcd(a,b-a);
     }
 
     public static int fibonacci(int n) {
-    	if(n==0)
-    		return 0;
-    	else if(n == 1)
-    		return 1;
-    	else
-    		return fibonacci(i-1)+fibonacci(i-2);
+        if(n==0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return fibonacci(n-1)+fibonacci(n-2);
     }
 }
--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,39 +1,84 @@
-package week2.task2;
+package week2;
 
 public class Fraction {
-
+    Task1 test = new Task1();
     private int numberator, denominator;
 
-    public Fraction(int numerator, int denominator) {       //contructor cho phan so
-        this.numerator = numberator;
+    public Fraction() {
+    }
+
+    public Fraction(int numberator, int denominator) {       //contructor cho phan so
+        this.numberator = numberator;
         this.denominator = denominator;
+    }
+    public Fraction Toigian(){
+        int Uoc = test.gcd(numberator,denominator);
+        this.numberator /= Uoc;
+        this.denominator/= Uoc;
+        return this;
     }
 
     public Fraction add(Fraction other) {
-        this.numerator = this.numerator*other.denominator+this.denominator*other.numerator;         
-        this.denominator = this.denominator*other.denominator;
-        return this;
+        Fraction sum = new Fraction();
+        sum.numberator = this.numberator*other.denominator+this.denominator*other.numberator;
+        sum.denominator = this.denominator*other.denominator;
+        sum.Toigian();
+        return sum;
     }
 
     public Fraction subtract(Fraction other) {
-        this.numerator = this.numerator*other.denominator-this.denominator*other.numerator;
-        this.denominator = this.denominator*other.denominator;
-        return this;
+        Fraction sub = new Fraction();
+        sub.numberator = this.numberator*other.denominator-this.denominator*other.numberator;
+        sub.denominator = this.denominator*other.denominator;
+        sub.Toigian();
+        return sub;
     }
 
     public Fraction multiply(Fraction other) {
-        this.numerator = this.numerator*other.numerator;
-        this.denominator = this.denominator*other.denominator;
-        return this;
+        Fraction multi = new Fraction();
+        multi.numberator = this.numberator*other.numberator;
+        multi.denominator = this.denominator*other.denominator;
+        multi.Toigian();
+        return multi;
     }
 
     public Fraction divide(Fraction other) {
-        this.numerator = this.numerator*other.denominator;
-        this.denominator = this.denominator*other.numerator;
-        return this;
+        Fraction div = new Fraction();
+        div.numberator = this.numberator*other.denominator;
+        div.denominator = this.denominator*other.numberator;
+        div.Toigian();
+        return div;
     }
 
+    public void Out(){
+        System.out.println("Phan so la: ");
+        if(denominator == 0)
+            System.out.println("Wrong!");
+        else if(denominator == 1)
+            System.out.println(numberator);
+        else if(denominator < 0){
+            if(numberator < 0)
+                System.out.println(numberator + "/" + denominator);
+            else
+                System.out.println(numberator*-1 + "/" + denominator*-1);
+        }
+        else
+            System.out.println(numberator + "/" + denominator);
+    }
     public boolean equals(Fraction other){
-        return (this.numerator*other.denominator == this.denominator*other.numberator);
+        return (this.numberator*other.denominator == this.denominator*other.numberator);
+    }
+
+    public static void main( String[] args){
+        Fraction frac1 = new Fraction(101,2);
+        Fraction frac2 = new Fraction(3,2);
+        Fraction sum = frac1.add(frac2);
+        Fraction subtr = frac1.subtract(frac2);
+        Fraction divi = frac1.divide(frac2);
+        Fraction mul = frac1.multiply(frac2);
+        sum.Out();
+        subtr.Out();
+        divi.Out();
+        mul.Out();
     }
 }
